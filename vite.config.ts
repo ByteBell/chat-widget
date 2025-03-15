ff53700248a2e06211ea38db18381ac48d3a4e13
Nitesh Sharma <nit8339@gmail.com>
2025-03-16 01:47:42 +0530

    mian chaninge

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: "src/main.tsx",
-      name: "ShadowButtonWidget",
-      formats: ["umd"],
-      fileName: () => "shadow-button-widget16.js",
-    },
-    minify: "terser",
-  },
+  // build: {
+  //   lib: {
+  //     entry: "src/main.tsx",
+  //     name: "ShadowButtonWidget",
+  //     formats: ["umd"],
+  //     fileName: () => "shadow-button-widget16.js",
+  //   },
+  //   minify: "terser",
+  // },
 });
