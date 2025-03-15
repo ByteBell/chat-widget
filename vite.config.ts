433a34a3b569444c5b3224681f183c81846e02c0
Nitesh Sharma <nit8339@gmail.com>
2025-03-16 01:56:25 +0530

    main fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    lib: {
-      entry: "src/main.tsx",
-      name: "ShadowButtonWidget",
-      formats: ["es"],
-      fileName: () => "shadow-button-widget18.js",
-    },
     minify: "terser",
   },
 });
