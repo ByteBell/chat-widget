fe878a9ae0476e4c1ef560ee9c37d0c78543332c
Nitesh Sharma <nit8339@gmail.com>
2025-03-16 01:01:36 +0530

    main 14

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,25 @@ export default defineConfig({
       entry: "src/main.tsx",
       name: "ShadowButtonWidget",
       formats: ["umd"],
-      fileName: () => "shadow-button-widget13.js",
+      fileName: () => "shadow-button-widget14.js",
     },
     minify: "terser",
+    rollupOptions: {
+      // Make sure to externalize dependencies you don't want to bundle
+      external: ["react", "react-dom"],
+      output: {
+        // Global variables to use in UMD build for externalized deps
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  define: {
+    // This explicitly replaces 'process.env.NODE_ENV' with 'production'
+    "process.env.NODE_ENV": JSON.stringify("production"),
+    // If you need other env variables:
+    // 'process.env': JSON.stringify({})
   },
 });
